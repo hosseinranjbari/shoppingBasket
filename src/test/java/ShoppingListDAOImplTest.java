b2e050c910af653c038ee090c91c3dbbd55dbabc
hosseinranjbari <ranjbari.hossein1997@gmail.com>
2024-01-19 00:19:01 +0330

    add a test for select the most quantity in item table

--- src/test/java/ShoppingListDAOImplTest.java
+++ src/test/java/ShoppingListDAOImplTest.java
@@ -1,13 +1,11 @@
 import org.assertj.core.api.Assertions;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import java.sql.Connection;
-import java.sql.DriverManager;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
 
-import static java.sql.DriverManager.*;
 import static java.sql.DriverManager.getConnection;
 
 public class ShoppingListDAOImplTest {
@@ -15,6 +13,7 @@ public class ShoppingListDAOImplTest {
     public static final String ERROR_MESSAGE = "There is a problem to connect to your DB";
     public static final String INSER_COMMAND = "INSERT INTO item (name, quantity) VALUES (?, ?)";
     public static final String UPDATE_COMMAND = "UPDATE item set quantity = 15 where id = 1";
+    public static final String SELECT_COMMAND = "SELECT * FROM item ORDER BY quantity DESC LIMIT 1";
 
     @Test
     void should_connect_to_MySQL_DB() {
@@ -67,6 +66,25 @@ public class ShoppingListDAOImplTest {
              final PreparedStatement deleteCommand = con.prepareStatement("DELETE FROM item where id = 4")
         ) {
             deleteCommand.executeUpdate();
+
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    @Test
+    void should_select_the_most_quantity_row() {
+        try (final Connection con = getConnection(DBConfig.MYSQL_DB_URL, DBConfig.MYSQL_DB_USER_NAME, DBConfig.MYSQL_DB_USER_PASS);
+             final PreparedStatement selectCommand = con.prepareStatement(SELECT_COMMAND);
+        ){
+            ResultSet resultSet = selectCommand.executeQuery();
+
+            while (resultSet.next()) {
+                String name = resultSet.getString("name");
+                int quantity = resultSet.getInt("quantity");
+                Assertions.assertThat(name).isEqualTo("apple");
+                Assertions.assertThat(quantity).isEqualTo(20);
+            }
 
         } catch (SQLException e) {
             throw new RuntimeException(e);
